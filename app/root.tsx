0c741818899c2207eb8c97d4ecbbfd635efc6d80
Abhijit47 <akarmakar846@gmail.com>
2026-02-05 12:48:25 +0530

    05-feat. Added light-dark mode

--- app/root.tsx
+++ app/root.tsx
@@ -5,14 +5,24 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
+  type LoaderFunctionArgs,
 } from 'react-router';
 
 import type { Route } from './+types/root';
 import './app.css';
-import { Toaster } from './components/ui/sonner';
 
 import 'mapbox-gl/dist/mapbox-gl.css';
+import { useEffect } from 'react';
+import {
+  PreventFlashOnWrongTheme,
+  ThemeProvider,
+  useTheme,
+} from 'remix-themes';
+import { Toaster } from './components/ui/sonner';
 import { StoreContextProvider } from './contexts/store-context';
+import { cn } from './lib/utils';
+import { themeSessionResolver } from './sessions.server';
 
 export const links: Route.LinksFunction = () => [
   { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
@@ -27,27 +37,81 @@ export const links: Route.LinksFunction = () => [
   },
 ];
 
-export function Layout({ children }: { children: React.ReactNode }) {
+// Return the theme from the session storage using the loader
+export async function loader({ request }: LoaderFunctionArgs) {
+  const { getTheme } = await themeSessionResolver(request);
+  return {
+    theme: getTheme(),
+  };
+}
+
+// HydrateFallback is rendered while the client loader is running
+export function HydrateFallback() {
+  return <div>Loading...</div>;
+}
+
+// export function Layout({ children }: { children: React.ReactNode }) {
+//   return (
+//     <html lang='en'>
+//       <head>
+//         <meta charSet='utf-8' />
+//         <meta name='viewport' content='width=device-width, initial-scale=1' />
+//         <Meta />
+//         <Links />
+//       </head>
+//       <body>
+//         <StoreContextProvider>{children}</StoreContextProvider>
+//         <ScrollRestoration />
+//         <Scripts />
+//         <Toaster />
+//       </body>
+//     </html>
+//   );
+// }
+
+// Wrap your app with ThemeProvider.
+// `specifiedTheme` is the stored theme in the session storage.
+// `themeAction` is the action name that's used to change the theme in the session storage.
+export default function AppWithProviders() {
+  const data = useLoaderData<typeof loader>();
+
+  useEffect(() => {
+    if (typeof window === undefined) return;
+  }, []);
+
   return (
-    <html lang='en'>
+    <ThemeProvider specifiedTheme={data.theme} themeAction='/action/set-theme'>
+      <StoreContextProvider>
+        <App />
+      </StoreContextProvider>
+    </ThemeProvider>
+  );
+}
+
+// export function App() {
+//   return <Outlet />;
+// }
+
+export function App() {
+  const data = useLoaderData<typeof loader>();
+  const [theme] = useTheme();
+  return (
+    <html lang='en' className={cn(theme)} suppressHydrationWarning>
       <head>
         <meta charSet='utf-8' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <Meta />
+        <PreventFlashOnWrongTheme ssrTheme={Boolean(data.theme)} />
         <Links />
       </head>
       <body>
-        <StoreContextProvider>{children}</StoreContextProvider>
+        <Outlet />
         <ScrollRestoration />
         <Scripts />
-        <Toaster />
+        <Toaster theme={theme ?? 'system'} richColors closeButton />
       </body>
     </html>
   );
-}
-
-export default function App() {
-  return <Outlet />;
 }
 
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
